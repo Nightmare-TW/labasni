a293606c6f4726e8542d9c0fbc41a386063addf4
medsaiefbardi <bardimohamedsaief@gmail.com>
2021-06-23 16:57:10 +0100

    create component / server

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Products, Navbar } from './components';
-
+import Create from './components/Products/Create.jsx';
 function App() {
   
   return (
     <div>
       <Navbar />
       <Products />
+      <Create/>
     </div>
   );
 }
